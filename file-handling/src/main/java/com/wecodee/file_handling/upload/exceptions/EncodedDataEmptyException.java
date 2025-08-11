3bce1022636cbe6fd46dce39d2c61b996d022338
Veeresh T A <veeresh.ta@wecodee.com>
2025-08-11 18:15:03 +0530

    required exception files added

---
+++ file-handling/src/main/java/com/wecodee/file_handling/upload/exceptions/EncodedDataEmptyException.java
@@ -0,0 +1,14 @@
+package com.wecodee.file_handling.upload.exceptions;
+
+import lombok.Getter;
+
+@Getter
+public class EncodedDataEmptyException extends RuntimeException {
+
+    public EncodedDataEmptyException(String message) {
+        super(message);
+        this.message = message;
+    }
+
+    private final String message;
+}
